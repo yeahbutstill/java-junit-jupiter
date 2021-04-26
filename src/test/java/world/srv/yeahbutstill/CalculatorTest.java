5ac59c780c4cb0efd84171dba4c1d16183f9060d
yeahbutstill <mayadani28@gmail.com>
2021-04-26 23:28:35 +0700

    menggunakan assertions

--- src/test/java/world/srv/yeahbutstill/CalculatorTest.java
+++ src/test/java/world/srv/yeahbutstill/CalculatorTest.java
@@ -1,6 +1,7 @@
 package world.srv.yeahbutstill;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -9,6 +10,21 @@ public class CalculatorTest {
     @Test
     public void testAddSucces() {
         var result = calculator.add(10,10);
+        // this is with Assertions
+        assertEquals(20, result);
+    }
+
+    @Test
+    public void testDiveideSucces() {
+        var result = calculator.devide(100, 10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDevideFailed() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.devide(10, 0);
+        });
     }
 
 }
