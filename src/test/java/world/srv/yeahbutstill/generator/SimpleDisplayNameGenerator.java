4112f6bf37769e41a48fcb2c76d05b1e5a47347a
yeahbutstill <mayadani28@gmail.com>
2021-04-27 00:26:02 +0700

    menggunakan display name generator

--- src/test/java/world/srv/yeahbutstill/generator/SimpleDisplayNameGenerator.java
+++ src/test/java/world/srv/yeahbutstill/generator/SimpleDisplayNameGenerator.java
@@ -1,2 +1,24 @@
-package world.srv.yeahbutstill.generator;public class SimpleDisplayNameGenerator {
+package world.srv.yeahbutstill.generator;
+
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+import java.lang.reflect.Method;
+
+public class SimpleDisplayNameGenerator implements DisplayNameGenerator {
+
+
+    @Override
+    public String generateDisplayNameForClass(Class<?> testClass) {
+        return "Test " + testClass.getSimpleName();
+    }
+
+    @Override
+    public String generateDisplayNameForNestedClass(Class<?> aClass) {
+        return null;
+    }
+
+    @Override
+    public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+        return "Test " + testClass.getSimpleName() + "." + testMethod.getName();
+    }
 }
